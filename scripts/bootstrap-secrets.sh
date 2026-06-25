817b6109a7bb4604924e339042b6808f2a37062f
Brian Richardson <brian@blackcircuit.ca>
2026-06-25 08:22:28 -0600

    add redirect URLs to MinIO initial environment

--- scripts/bootstrap-secrets.sh
+++ scripts/bootstrap-secrets.sh
@@ -341,6 +341,8 @@ function create_minio_secret() {
     export MINIO_ACCESS_KEY="minio-root-$(openssl rand -hex 8)"
     export MINIO_SECRET_KEY="$(openssl rand -base64 48 | tr -d '\n')"
     export MINIO_IDENTITY_OPENID_CONFIG_URL="https://sso-dev.int.aethericforge.ca/realms/int.aethericforge.ca/.well-known/openid-configuration"
+    export MINIO_BROWSER_REDIRECT_URL="https://console-dev.int.aethericforge.ca"
+    export MINIO_SERVER_URL="https://s3-dev.int.aethericforge.ca"
     export MINIO_IDENTITY_OPENID_CLIENT_ID="minio"
     export MINIO_IDENTITY_OPENID_CLIENT_SECRET="$(openssl rand -base64 48 | tr -d '\n')"
     export MINIO_IDENTITY_OPENID_DISPLAY_NAME="MinIO"
